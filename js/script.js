86f0d84c27e2adbaa5650536bfc3e58368bb9a25
Jack-Reid <81870424+Jack-Reid@users.noreply.github.com>
2026-06-22 11:40:57 +0100

    Reformatting link for email address

--- js/script.js
+++ js/script.js
@@ -8,6 +8,12 @@ function expand() {
 	return x
 }
 
+function gen_mailto_link(site,user) {
+	document.write("<a href=\"mailto");
+	document.write(":" + user + "@");
+	document.write(site + "\">" + user + "@" + site + "<\/a>");
+}
+
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event=>document.body.classList.toggle('dark-mode'))
 
 window.onload = function() {if (window.matchMedia('(prefers-color-scheme: dark)').matches) {document.body.classList.add('dark-mode')} else {document.body.classList.remove('dark-mode')}};
